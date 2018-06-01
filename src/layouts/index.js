6bef0768bb8cd0f74f6ec4c9b90af10e923dc814
Josh Owens <joshua.owens@gmail.com>
2018-05-31 22:31:09 -0400

    Social info

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,8 +16,21 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.name}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content:'Javascript is one of the fastest growing languages in the world, and is the most popular langauge on Github. Get up to speed or sharpen your existing javascript knowledge with these free talks.'},
+        { name: 'keywords', content: 'javascript, es2016, node.js, angular.js, react.js, vue.js, feathers.js' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content:"anywherejs" },
+        { name: 'twitter:title', content: 'Feeling crunched for learning time?' },
+        { name: 'twitter:description', content:'Sign up, plug in, and level up with this meetup! Get the very best talks, all without the travel.' },
+        { name: 'twitter:image:src', content: 'http://conf.crater.io/images/crater-remote-conf.jpeg' },
+        { property: 'og:title', content: 'Feeling crunched for learning time?' },
+        { property: 'og:site_name', content: 'AnywhereJS - The remote meetup for the world' },
+        { property: 'og:url', content: 'http://anywherejs.com/' },
+        { property: 'og:description', content: 'Sign up, plug in, and level up with this meetup! Get the very best talks, all without the travel.' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: 'http://conf.crater.io/images/crater-remote-conf.jpeg' },
+        { property: 'og:image:height', content: '1890' },
+        { property: 'og:image:width', content: '3000' }
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.name} />
