e7d3df0d5aa95437f8c791514da881a732ed70b0
Josh Owens <joshua.owens@gmail.com>
2018-06-14 22:44:16 -0400

    Add a thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './index.css'
+
+const ThanksPage = ({data}) => {
+  return (
+    <div>
+      Thanks for your interest, we will send you information on the next event soon.
+    </div>
+  )
+}
+
+export default ThanksPage
